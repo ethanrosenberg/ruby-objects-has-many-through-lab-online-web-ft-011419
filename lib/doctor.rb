0fb0fe78830204b047477fa4816fd585e86a86bd
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-04 10:22:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
     @@all
   end
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     appt = Appointment.new(date, patient, self)
     @appointments << appt
     @patients << patient
